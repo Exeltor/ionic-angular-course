e6e9652f325a92d957eb57556393db420bba855d
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-07-16 23:49:08 +0200

    Haciendo la Reactive Form para newOffer

--- ionic-angular-course/src/app/places/offers/new-offer/new-offer.page.ts
+++ ionic-angular-course/src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  // Se requiere crear una form con FormGroup para forms reactivas
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    // Las forms reactivas se configuran solo dentro de ngOnInit
+    this.form = new FormGroup({
+      // Declaramos los elementos como FormControl y los ajustamos
+      title: new FormControl(null, {
+        // 'blur' para cuando pierde focus
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  onCreateOffer() {
+    console.log('Creating offered place');
   }
 
 }
