cbb4d13134dd5a63fe92f9ddfda699fc9bdae117
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-07-20 13:24:29 +0200

    Modificando onCreateOffer()

--- ionic-angular-course/src/app/places/places.service.ts
+++ ionic-angular-course/src/app/places/places.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Place } from './place.model'
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class PlacesService {
       'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
       149.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       'p2',
@@ -22,7 +24,8 @@ export class PlacesService {
       'https://i.ytimg.com/vi/NQv9Z-LZ618/hqdefault.jpg',
       189.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       'p3',
@@ -31,7 +34,8 @@ export class PlacesService {
       'https://previews.123rf.com/images/andrascsontos/andrascsontos1609/andrascsontos160900120/63752595-foggy-old-stairway-of-a-palace.jpg',
       99.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date('2019-12-31'),
+      'abc'
     )
 
 
@@ -41,10 +45,24 @@ export class PlacesService {
     return[...this._places];
   }
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   getPlace(id: string) {
     // Clonamos el objeto con "..." al principio, lo buscamos con .find segun el criterio de que las id's sean iguales
     return {...this._places.find(p => p.id === id)};
+  }
+
+  addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
+    const newPlace = new Place(
+      Math.random().toString(),
+      title,
+      description,
+      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId);
+
+      this.places.push(newPlace);
   }
 }
