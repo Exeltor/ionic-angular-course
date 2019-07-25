ecd13c455ff02b6ef7028be9a190a1294a56e2a5
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-07-26 00:36:25 +0200

    Doing image picker

--- ionic-angular-course/src/app/places/offers/new-offer/new-offer.page.ts
+++ ionic-angular-course/src/app/places/offers/new-offer/new-offer.page.ts
@@ -4,6 +4,28 @@ import { PlacesService } from '../../places.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { PlaceLocation } from '../../location.model';
+import { ArrayType } from '@angular/compiler';
+
+function base64toBlob(base64Data, contentType) {
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = window.atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+    const begin = sliceIndex * sliceSize;
+    const end = Math.min(begin + sliceSize, bytesLength);
+
+    const bytes = new Array(end - begin);
+    for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+      bytes[i] = byteCharacters[offset].charCodeAt(0);
+    }
+    byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, {type: contentType});
+}
 
 @Component({
   selector: 'app-new-offer',
@@ -43,14 +65,31 @@ export class NewOfferPage implements OnInit {
       }),
       location: new FormControl(null, {
         validators: [Validators.required]
-      })
+      }),
+      image: new FormControl(null)
     });
   }
 
   onLocationPicked(location: PlaceLocation) {
     this.form.patchValue({
-      location: location
+      location
     });
+  }
+
+  onImagePicked(imageData: string | File) {
+    let imageFile;
+    if (typeof imageData === 'string') {
+      try {
+        imageFile = base64toBlob(imageData.replace('data:image/jpeg;base64,', ''), 'image/jpeg');
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    } else {
+      imageFile = imageData;
+    }
+
+    this.form.patchValue({image: imageFile});
   }
 
   onCreateOffer() {
--- ionic-angular-course/src/app/shared/pickers/location-picker/location-picker.component.ts
+++ ionic-angular-course/src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ActionSheetController, AlertController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { map, switchMap } from 'rxjs/operators';
-import { PlaceLocation } from 'src/app/places/location.model';
+import { PlaceLocation, Coordinates } from 'src/app/places/location.model';
 import { of } from 'rxjs';
+import { Plugins, Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-location-picker',
@@ -16,12 +17,39 @@ export class LocationPickerComponent implements OnInit {
   @Output() locationPick = new EventEmitter<PlaceLocation>();
   selectedLocationImage: string;
   isLoading = false;
+  private alertCtrl: AlertController;
 
-  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
+  constructor(private modalCtrl: ModalController, private http: HttpClient, private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
 
   onPickLocation() {
+    this.actionSheetCtrl.create({
+      header: 'Please choose',
+      buttons: [
+        {
+          text: 'Auto-Locate',
+          handler: () => {
+            this.locateUser();
+          }
+        },
+        {
+          text: 'Pick on Map',
+          handler: () => {
+            this.openMap();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    }).then(actionEl => {
+      actionEl.present();
+    });
+  }
+
+  private openMap() {
     this.modalCtrl.create({
       component: MapModalComponent
     }).then(modalEl => {
@@ -30,26 +58,58 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        const pickedLocation: PlaceLocation = {
+        const coordinates: Coordinates = {
           lat: modalData.data.lat,
-          lng: modalData.data.lng,
-          address: null,
-          staticMapImageUrl: null
+          lng: modalData.data.lng
         };
-        this.isLoading = true;
-        // Si hay dato, obtenemos direccion
-        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(switchMap(address => {
-          pickedLocation.address = address;
-          return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
-        })).subscribe(staticMapImageUrl => {
-          pickedLocation.staticMapImageUrl = staticMapImageUrl;
-          this.selectedLocationImage = staticMapImageUrl;
-          this.isLoading = false;
-          this.locationPick.emit(pickedLocation);
-        });
+
+        this.createPlace(coordinates.lat, coordinates.lng);
       });
       modalEl.present();
     });
+  }
+
+  private createPlace(lat: number, lng: number) {
+    const pickedLocation: PlaceLocation = {
+      lat,
+      lng,
+      address: null,
+      staticMapImageUrl: null
+    };
+    this.isLoading = true;
+    // Si hay dato, obtenemos direccion
+    this.getAddress(lat, lng).pipe(switchMap(address => {
+      pickedLocation.address = address;
+      return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+    })).subscribe(staticMapImageUrl => {
+      pickedLocation.staticMapImageUrl = staticMapImageUrl;
+      this.selectedLocationImage = staticMapImageUrl;
+      this.isLoading = false;
+      this.locationPick.emit(pickedLocation);
+    });
+  }
+
+  private locateUser() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.showErrorAlert();
+      return;
+    }
+
+    Plugins.Geolocation.getCurrentPosition().then(geoPosition => {
+      const coordinates: Coordinates = {lat: geoPosition.coords.latitude, lng: geoPosition.coords.longitude};
+      this.createPlace(coordinates.lat, coordinates.lng);
+    }).catch(err => {
+      this.showErrorAlert();
+    });
+
+  }
+
+  private showErrorAlert() {
+    this.alertCtrl.create({
+      header: 'Could not fetch location',
+      message: 'Please use the map to pick a location',
+      buttons: ['Okay']
+    }).then(alertEl => alertEl.present());
   }
 
   private getAddress(lat: number, lng: number) {
