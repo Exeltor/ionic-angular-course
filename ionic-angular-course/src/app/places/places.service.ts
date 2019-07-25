4c4ef1d3f45feea285e4a0bef4e5fd688933f8dc
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-07-25 20:52:23 +0200

    Builds for ios and android

--- ionic-angular-course/src/app/places/places.service.ts
+++ ionic-angular-course/src/app/places/places.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Place } from './place.model'
+import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
 import { BehaviorSubject, of } from 'rxjs';
 import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { PlaceLocation } from './location.model';
 
 interface PlaceData {
   availableFrom: string;
@@ -13,6 +14,7 @@ interface PlaceData {
   price: number;
   title: string;
   userId: string;
+  location: PlaceLocation;
 }
 
 @Injectable({
@@ -47,7 +49,8 @@ export class PlacesService {
               resData[key].price,
               new Date(resData[key].availableFrom),
               new Date(resData[key].availableTo),
-              resData[key].userId
+              resData[key].userId,
+              resData[key].location
             )
           );
         }
@@ -75,12 +78,14 @@ export class PlacesService {
           placeData.price,
           new Date(placeData.availableFrom),
           new Date(placeData.availableTo),
-          placeData.userId);
+          placeData.userId,
+          placeData.location
+        );
       })
     );
   }
 
-  addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
+  addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date, location: PlaceLocation) {
     let generatedId: string;
     const newPlace = new Place(
       Math.random().toString(),
@@ -90,7 +95,8 @@ export class PlacesService {
       price,
       dateFrom,
       dateTo,
-      this.authService.userId
+      this.authService.userId,
+      location
     );
 
     // Elegimos la tabla despues del '/' (en este caso 'offered-places'), igual que en MongoDB
@@ -142,7 +148,8 @@ export class PlacesService {
         oldPlace.price,
         oldPlace.availableFrom,
         oldPlace.availableTo,
-        oldPlace.userId
+        oldPlace.userId,
+        oldPlace.location
       );
       return this.http.put(
         `https://ionic-angular-course-a45fb.firebaseio.com/offered-places/${placeId}.json`,
--- ionic-angular-course/src/app/shared/map-modal/map-modal.component.ts
+++ ionic-angular-course/src/app/shared/map-modal/map-modal.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, Renderer2, OnDestroy } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, Renderer2, OnDestroy, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 import { environment } from '../../../environments/environment';
+import { MapOperator } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-map-modal',
@@ -10,6 +11,10 @@ import { environment } from '../../../environments/environment';
 })
 export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('map') mapElementRef: ElementRef;
+  @Input() center = { lat: -34.397 , lng: 150.644 };
+  @Input() selectable = true;
+  @Input() closeButtonText = 'Cancel';
+  @Input() title = 'Pick Location';
   clickListener: any;
   googleMaps: any;
 
@@ -22,7 +27,7 @@ export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
       this.googleMaps = googleMaps;
       const mapEl = this.mapElementRef.nativeElement;
       const map = new googleMaps.Map(mapEl, {
-        center: { lat: -34.397 , lng: 150.644 },
+        center: this.center,
         zoom: 16
       });
 
@@ -30,11 +35,20 @@ export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
         this.renderer.addClass(mapEl, 'visible');
       });
 
-      this.clickListener = map.addListener('click', event => {
-        const selectedCoords = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-        this.modalCtrl.dismiss(selectedCoords);
-      });
+      if (this.selectable) {
+        this.clickListener = map.addListener('click', event => {
+          const selectedCoords = { lat: event.latLng.lat(), lng: event.latLng.lng() };
+          this.modalCtrl.dismiss(selectedCoords);
+        });
+      } else {
+        const marker = new googleMaps.Marker({
+          position: this.center,
+          map: map,
+          title: 'Picked Location'
+        });
 
+        marker.setMap(map);
+      }
     }).catch(err => {
       console.log(err);
     });
@@ -71,7 +85,9 @@ export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.googleMaps.event.removeListener(this.clickListener);
+    if (this.clickListener) {
+      this.googleMaps.event.removeListener(this.clickListener);
+    }
   }
 
 }
